7df0f62add832081f2ba21e8d1656561530f5b7c
Ben Page <ben.page@gmail.com>
2017-07-14 16:14:50 -0400

    Switched from simple auth to hmac

--- bolt-app-sdk.go
+++ bolt-app-sdk.go
@@ -2,44 +2,75 @@ package boltAppSdk
 
 import (
 	"bytes"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/TeamFairmont/boltshared/security"
 )
 
 //AppFunc is the app function to be passed in
 type AppFunc func(map[string]interface{}) error
 
-//RunApp takes a function and handles the bolt communication.
+//RunApp takes a function and handles the bolt communication
 func RunApp(boltURL, userName, passWord string, af AppFunc) error {
 	var payload = make(map[string]interface{})
 	//run app function
 	err := af(payload)
 	if err != nil {
-		fmt.Println(`error in app funbction `, err)
-		return err
-	}
-	//mashal the payload into json object
-	p, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println("eeror Marshalling json ", err)
+		fmt.Println(`error in app function: `, err)
 		return err
 	}
 
-	req, err := http.NewRequest("POST", boltURL, bytes.NewBuffer(p))
+	//marshal the payload into json object
+	p, err := json.Marshal(payload)
 	if err != nil {
-		fmt.Println("error making http request", err)
+		fmt.Println("error marshalling json: ", err)
 		return err
 	}
+
+	// Prepare the payload with hmac encoding
+	// Encode the message to send
+	hmacToSend, err := security.EncodeHMAC(
+		passWord,
+		string(p),
+		strconv.FormatInt(time.Now().Unix(), 10),
+	)
+	if err != nil {
+		fmt.Println(`error hmac encoding payload: `, err)
+		return err
+	}
+
+	req, err := http.NewRequest("POST", boltURL, bytes.NewBuffer(hmacToSend))
+	if err != nil {
+		fmt.Println("error making http request: ", err)
+		return err
+	}
+
+	// The tls.Config settings are set server side, but may also be set client side.
+	// InsecureSkipVerify allows self-signed certificates in development environments.
+	// This must be set to false for production using a trusted certificate authority.
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+			MinVersion:         tls.VersionTLS12, // Communicate with TLS 1.2 (771)    	PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			},
+		},
+		DisableCompression: true, // Compressed TLS is vulnerable to attacks
+	}
+	client := &http.Client{Transport: tr}
+
 	//set auth and header
-	req.SetBasicAuth(userName, passWord)
-	req.Header.Set("Content-Type", "application/json")
-	//send request
-	client := &http.Client{}
+	req.SetBasicAuth(userName, "pw ignored")
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("error doing client: ", err)
+		fmt.Println("error in client.Do: ", err)
 		return err
 	}
 	defer resp.Body.Close()
